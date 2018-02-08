b458ffe1d9ce488907a49bb749cb5cf826a2ac70
Guillermo Escobero <100346060@alumnos.uc3m.es>
2018-02-08 17:13:28 +0100

    First implementation of printFile.c

--- person.h
+++ person.h
@@ -1,1 +1,12 @@
+#ifndef PERSON_H
+#define PERSON_H
 
+struct person {
+    char name[48];
+    uint32_t age;
+    uint32_t id;
+    char control;
+    float salary;
+};
+
+#endif /* PERSON_H */
--- printFile.c
+++ printFile.c
@@ -1,7 +1,60 @@
-#include "persona.h"
+#include "person.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int readBinary(char* inputFile);
 
 int main(int argc, char *argv[]){
+    if(argc != 2) {
+        return 1;
+    }
+    
+    //readBinary(argv[1]);
 	
-	return 0;
+    /*int inputFileDesc = open(argv[1], O_RDONLY);
+    
+    if(inputFileDesc < 0)
+        return 1;
+
+    if(close(inputFileDesc) < 0)
+        return 1;
+    */
+	return readBinary(argv[1]);
+}
+
+int readBinary(char* inputFile) {
+    struct person input;
+
+    int inputFileDesc = open(inputFile, O_RDONLY);
+    if(inputFileDesc < 0)
+        return 1;
+
+    struct stat st;
+    stat(inputFile, &st);
+    long binSize = st.st_size;
+
+    long fileRows = binSize/(sizeof(input.name) + sizeof(input.age) + 
+            sizeof(input.id) + sizeof(input.control) + sizeof(input.salary));
+
+    lseek(inputFileDesc, 0, SEEK_SET);
+
+    int i;
+
+    for(i = 0; i < fileRows; i++) {
+        read(inputFileDesc, &input.name, sizeof(input.name));
+        read(inputFileDesc, &input.age, sizeof(input.age));
+        read(inputFileDesc, &input.id, sizeof(input.id));
+        read(inputFileDesc, &input.control, sizeof(input.control));
+        read(inputFileDesc, &input.salary, sizeof(input.salary));
+
+        printf("%s\t%d\t%d\t%c\t%f\n", input.name, input.age, input.id, input.control, input.salary);
+    }
+
+    close(inputFileDesc);
+
+    return 0;
+
 }
 

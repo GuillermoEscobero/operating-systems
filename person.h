481650f36d4d31498a9faa874968aaf5e7dd45d6
Guillermo Escobero <100346060@alumnos.uc3m.es>
2018-02-17 18:14:11 +0100

    New supporting code

--- person.h
+++ person.h
@@ -1,14 +1,15 @@
-#ifndef PERSON_H
-#define PERSON_H
+/**
+ * Definition of personal data for PA1 - Operating Systems 17/18 
+ * 
+ * DO NOT MODIFY
+ *
+ * Data types valid as of Feb 2018 @guernika.lab.inf.uc3m.es with GCC (Debian 6.3.0-18) 6.3.0 20170516
+ */
 
-#include <stdint.h>
-
-struct person {
-    char name[48];
-    uint32_t age;
-    uint32_t id;
-    char control;
-    float salary;
-};
-
-#endif /* PERSON_H */
+typedef struct {
+  char          name[48];  /* 48-byte string */
+  unsigned int  age;       /* 4-byte unsigned integer between 0 and 150, included */
+  unsigned int  id;        /* 4-byte unsigned integer between 0 and 99999999, included */
+  char          id_ctrl;   /* Character */
+  double        salary;    /* 8-byte decimal between 0.0 and 100000.0, included */
+} Person;
--- printFile.c
+++ printFile.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[]){
     if(argc != 2) {
         return 1;
     }
-    
+
 	return readBinary(argv[1]);
 }
 
 int readBinary(char* inputFile) {
     /* Declaration of auxiliary person struct */
-    struct person input;
+    Person input;
 
     /* We get the file descriptor of the input file */
     int inputFileDesc = open(inputFile, O_RDONLY);
@@ -31,10 +31,10 @@ int readBinary(char* inputFile) {
     stat(inputFile, &st);
     long binSize = st.st_size;
 
-    /* We sum the size of each field separately to avoid padding symbols for 
+    /* We sum the size of each field separately to avoid padding symbols for
      * align purposes */
-    long fileRows = binSize/(sizeof(input.name) + sizeof(input.age) + 
-            sizeof(input.id) + sizeof(input.control) + sizeof(input.salary));
+    long fileRows = binSize/(sizeof(input.name) + sizeof(input.age) +
+            sizeof(input.id) + sizeof(input.id_ctrl) + sizeof(input.salary));
 
     /* Looks for the first location of the input file in memory */
     lseek(inputFileDesc, 0, SEEK_SET);
@@ -49,18 +49,17 @@ int readBinary(char* inputFile) {
         read(inputFileDesc, &input.name, sizeof(input.name));
         read(inputFileDesc, &input.age, sizeof(input.age));
         read(inputFileDesc, &input.id, sizeof(input.id));
-        read(inputFileDesc, &input.control, sizeof(input.control));
+        read(inputFileDesc, &input.id_ctrl, sizeof(input.id_ctrl));
         read(inputFileDesc, &input.salary, sizeof(input.salary));
 
         /* Prints in std output the current entry of the binary file */
-        printf("%s\t%d\t%.8u\t%c\t%.0f\n", input.name, input.age, input.id, 
-                input.control, input.salary);
+        printf("%s\t%d\t%.8u\t%c\t%.0f\n", input.name, input.age, input.id,
+                input.id_ctrl, input.salary);
     }
-    
+
     /* Close input file */
     close(inputFileDesc);
 
     return 0;
 
 }
-

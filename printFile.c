710c88514ed20ff20926e217aa8885a526d419b8
Guille Escobero <GuillermoEscobero@users.noreply.github.com>
2018-02-19 12:11:24 +0100

    Printfile (#4)
    * First implementation of printFile.c
    
    * Commented file
    
    * Fix includes and uint data
    
    * New supporting code
    
    * Minor improvements printFile.c

--- printFile.c
+++ printFile.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[]){
     if(argc != 2) {
         return 1;
     }
-    
+  
 	return readBinary(argv[1]);
 }
 
 int readBinary(char* inputFile) {
     /* Declaration of auxiliary person struct */
-    struct person input;
+    Person input;
 
     /* We get the file descriptor of the input file */
     int inputFileDesc = open(inputFile, O_RDONLY);
@@ -31,10 +31,8 @@ int readBinary(char* inputFile) {
     stat(inputFile, &st);
     long binSize = st.st_size;
 
-    /* We sum the size of each field separately to avoid padding symbols for 
-     * align purposes */
-    long fileRows = binSize/(sizeof(input.name) + sizeof(input.age) + 
-            sizeof(input.id) + sizeof(input.control) + sizeof(input.salary));
+    /* We calculate the number of entries that the binary file has */
+    long fileRows = binSize/sizeof(Person);
 
     /* Looks for the first location of the input file in memory */
     lseek(inputFileDesc, 0, SEEK_SET);
@@ -44,23 +42,18 @@ int readBinary(char* inputFile) {
     /* This loop will repeat the number of structs contained in the binary
      * file */
     for(i = 0; i < fileRows; i++) {
-        /* These lines read each field of the current struct in the file
-         * and save the value in the struct input */
-        read(inputFileDesc, &input.name, sizeof(input.name));
-        read(inputFileDesc, &input.age, sizeof(input.age));
-        read(inputFileDesc, &input.id, sizeof(input.id));
-        read(inputFileDesc, &input.control, sizeof(input.control));
-        read(inputFileDesc, &input.salary, sizeof(input.salary));
+        /* This line read each struct in the file and save the value in the
+         * auxiliary struct input */
+        read(inputFileDesc, &input, sizeof(Person));
 
         /* Prints in std output the current entry of the binary file */
-        printf("%s\t%d\t%.8u\t%c\t%.0f\n", input.name, input.age, input.id, 
-                input.control, input.salary);
+        printf("%s\t%d\t%.8u\t%c\t%.0f\n", input.name, input.age, input.id,
+                input.id_ctrl, input.salary);
     }
-    
+
     /* Close input file */
     close(inputFileDesc);
 
     return 0;
 
 }
-

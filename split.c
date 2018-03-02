0553aac8e1b67e55b3110fe6487cffdbdeb506cc
Raul Olmedo <raul.olmedo.dev@gmail.com>
2018-03-02 17:10:58 +0100

    Added error handling

--- split.c
+++ split.c
@@ -3,27 +3,42 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "person.h"
 
 int splitBinary(char *inputFile, char *youngerOutputFile, char *olderOutputFile, int thresholdAge);
 
 
 int main(int argc, char *argv[]) {
-    /* TODO: handle errors */
-    return splitBinary(argv[2], argv[3], argv[4], atoi(argv[1]));
+    if (argc != 5) {
+        printf("Error: Wrong arguments number");
+        return -1;
+    }
+
+    return splitBinary(argv[2], argv[3], argv[4], (int) strtol(argv[1], NULL, 10));
+
 }
 
 int splitBinary(char *inputFile, char *youngerOutputFile, char *olderOutputFile, int thresholdAge) {
     Person auxPerson;
 
-    /* We get the file descriptor of the input file */
+    /* We get the input file descriptor of the input file with a read only file permission */
     int fdInput = open(inputFile, O_RDONLY);
+
+    /* We open both output files in order to store the information, the flags allow the creation of the
+     * file or addition at the end to a pre existing one */
     int fdYoungerOutput = open(youngerOutputFile, O_CREAT | O_APPEND | O_WRONLY,
                                S_IRWXU + S_IRWXG + S_IRWXO);
     int fdOlderOutput = open(olderOutputFile, O_CREAT | O_APPEND | O_WRONLY, S_IRWXU + S_IRWXG + S_IRWXO);
 
     /* Check if an error ocurred when opening file */
-    if (fdInput < 0 || fdYoungerOutput < 0 || fdOlderOutput < 0) {
+    if (fdInput < 0) {
+        printf("Error: No such file or directory");
+        return -1;
+    }
+
+    if (fdYoungerOutput < 0 || fdOlderOutput < 0) {
+        printf("Error: Output files creation failed");
         return -1;
     }
 
@@ -36,6 +51,11 @@ int splitBinary(char *inputFile, char *youngerOutputFile, char *olderOutputFile,
     /* We calculate the number of entries that the binary file has */
     long fileRows = binSize / sizeof(Person);
 
+    if (fileRows == 0) {
+        printf("Error: Input file is empty");
+        return -1;
+    }
+
     /* Looks for the first location of the input file in memory */
     lseek(fdInput, 0, SEEK_SET);
 
@@ -43,21 +63,20 @@ int splitBinary(char *inputFile, char *youngerOutputFile, char *olderOutputFile,
      * file */
     int i;
     for (i = 0; i < fileRows; i++) {
-        /* This line read each struct in the file and save the value in the
-         * auxiliary struct input */
-
+        /* We read each struct in the file and save the value in the
+         * auxiliary struct input to be compared later */
         read(fdInput, &auxPerson, sizeof(auxPerson));
 
         if (auxPerson.age < thresholdAge) {
-            /* fill the younger file */
+            /* If the age of the read person is below the threshold we write it in the corresponding file*/
             write(fdYoungerOutput, &auxPerson, sizeof(auxPerson));
         } else {
-            /* fill the older file */
+            /* Fill the other file if the condition was not fulfilled */
             write(fdOlderOutput, &auxPerson, sizeof(auxPerson));
         }
     }
 
-    /* Close input file */
+    /* Close input and output files */
     close(fdInput);
     close(fdOlderOutput);
     close(fdYoungerOutput);

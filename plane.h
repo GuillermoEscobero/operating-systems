ebfb23826dd0cbb4562c3bb501ae618a596b46f9
Guille Escobero <GuillermoEscobero@users.noreply.github.com>
2018-04-13 17:52:51 +0200

    Added initial material for PA3

---
+++ plane.h
@@ -0,0 +1,17 @@
+//
+// THIS FILE CANNOT BE MODIFIED
+//
+
+#ifndef ARCPORT_PLANE_H
+#define ARCPORT_PLANE_H
+
+
+struct plane {
+    int id_number;
+    int time_action;
+    int action;
+    int last_flight; 
+};
+
+
+#endif //ARCPORT_PLANE_H

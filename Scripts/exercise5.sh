e08685e617bcb6357f5641c35ff4911cf351870d
RaulOlmedoCheca <raul.olmedo.checa@gmail.com>
2018-04-27 16:06:11 +0200

    Rename exercise 5 + comment the added code

--- Scripts/exercise5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-WHAT=$(zenity --list --checklist --column Yes/No --column Item TRUE usr/include/ FALSE usr/share/backgrounds/ FALSE usr/share/pixmaps/ TRUE usr/share/man/ | tr \| \ )
-SIZE=$(stat --printf=%s $0)
-if [ -n "$WHAT" ]; then
-  (tail -n +8 $0 | pv -n -s $SIZE -i 0.25 | base64 -d | tar -xzf - $WHAT) 2>&1 | zenity --progress --text=Descomprimiendo... --auto-close --auto-kill --time-remaining
-fi
-exit 0;

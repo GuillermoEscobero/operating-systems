b5b763efe734214df7b69abab5cf44fd619e38db
Guillermo Escobero <100346060@alumnos.uc3m.es>
2018-04-27 20:02:50 +0200

    First approach protected queue, track_manager and radar

--- arcport.c
+++ arcport.c
@@ -6,7 +6,25 @@
 
 #define NUM_TRACKS 1
 
-pthread_mutex_t mut_id;
+#define OP_TAKEOFF 0
+#define OP_LAND 1
+
+#define DEFAULT_PLANES_TAKEOFF 4
+#define DEFAULT_PLANES_LAND 3
+#define DEFAULT_TIME_TAKEOFF 2
+#define DEFAULT_TIME_LANDING 3
+#define DEFAULT_SIZE 6
+
+pthread_mutex_t mut_id = PTHREAD_MUTEX_INITIALIZER;
+
+int planes_takeoff = DEFAULT_PLANES_TAKEOFF;
+int time_takeoff = DEFAULT_TIME_TAKEOFF;
+int planes_land = DEFAULT_PLANES_LAND;
+int time_landing = DEFAULT_TIME_LANDING;
+int size = DEFAULT_SIZE;
+
+int next_id = 0;
+int waiting = 0;
 
 void print_banner()
 {
@@ -21,10 +39,111 @@ void print_end()
              "********************************************\n");
 }
 
+int track_manager(int n) {
+  int i;
+  struct plane *pln = (struct plane*)malloc(sizeof(struct plane));
+
+  for (i = 0; i < n; i++) {
+    pthread_mutex_lock(&mut_id);
+
+    pln->id_number = next_id;
+    pln->time_action = time_takeoff;
+    pln->action = OP_TAKEOFF;
+
+    if (waiting == 1) {
+      pln->last_flight = 1;
+    } else {
+      pln->last_flight = 0;
+    }
+
+    printf("[TRACKBOSS] Plane with id %d checked\n", pln->id_number);
+    queue_put(pln);
+    sleep(pln->time_action);
+    printf("[TRACKBOSS] Plane with id %d ready to takeoff\n", pln->id_number);
+
+    waiting--;
+    next_id++;
+
+    pthread_mutex_unlock(&mut_id);
+  }
+  return 0;
+}
+
+int radar(int n) {
+  int i;
+  struct plane *pln = (struct plane*)malloc(sizeof(struct plane));
+
+  for (i = 0; i < n; i++) {
+    pthread_mutex_lock(&mut_id);
+
+    pln->id_number = next_id;
+    pln->time_action = time_landing;
+    pln->action = OP_LAND;
+
+    if (waiting == 1) {
+      pln->last_flight = 1;
+    } else {
+      pln->last_flight = 0;
+    }
+
+    printf("[RADAR] Plane with id %d detected!\n", pln->id_number);
+    queue_put(pln);
+    sleep(pln->time_action);
+    printf("[RADAR] Plane with id %d ready to land\n", pln->id_number);
+
+    waiting--;
+    next_id++;
+
+    pthread_mutex_unlock(&mut_id);
+  }
+  return 0;
+}
+
 int main(int argc, char ** argv) {
+    waiting = planes_takeoff + planes_land;
 
     print_banner();
-	
+
+    /* Testing lines while implementing */
+    // queue_init (5);
+    // printf("queue_empty() = %d\n", queue_empty());
+    // printf("queue_full() = %d\n", queue_full());
+    //
+    // struct plane iberia = {
+    //   .id_number = 123,
+    //   .time_action = 5548,
+    //   .action = 4,
+    //   .last_flight = 556,
+    // };
+    //
+    // struct plane *caramel;
+    // queue_put (&iberia);
+    // printf("queue_empty() = %d\n", queue_empty());
+    // printf("queue_full() = %d\n", queue_full());
+    // display_queue();
+    // caramel = queue_get();
+    // printf("queue_empty() = %d\n", queue_empty());
+    // printf("queue_full() = %d\n", queue_full());
+    //
+    // printf("\nPLANE caramel\n");
+    // printf("id_number = %d\n", caramel->id_number);
+    // printf("time_action = %d\n", caramel->time_action);
+    // printf("action = %d\n", caramel->action);
+    // printf("last_flight = %d\n", caramel->last_flight);
+    // queue_destroy();
+
+    /* FINISH TESTING LINES */
+
+    queue_init(DEFAULT_SIZE);
+
+    pthread_t pid[3];
+
+    pthread_create(&pid[0], NULL, (void*)track_manager, (void*)DEFAULT_PLANES_TAKEOFF);
+    pthread_create(&pid[1], NULL, (void*)radar, (void*)DEFAULT_PLANES_LAND);
+
+    pthread_join(pid[0], NULL);
+    pthread_join(pid[1], NULL);
+
 	print_end();
 
     return 0;
--- queue.c
+++ queue.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include "queue.h"
 
 typedef struct queue {
@@ -12,6 +13,11 @@ typedef struct queue {
 
 Queue *q;
 
+pthread_mutex_t queue_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t queue_cond = PTHREAD_COND_INITIALIZER;
+
+int flag = 0;
+
 /*To create a queue*/
 int queue_init(int size){
         q = (Queue*)malloc(sizeof(Queue));
@@ -20,41 +26,63 @@ int queue_init(int size){
         q->rear = -1;
 
         q->size = size;
-        q->elements = (struct plane**)malloc(sizeof(struct plane**));
+        q->elements = (struct plane**)malloc(sizeof(struct plane*));
 
         for (int i = 0; i < size; i++) {
-          q->elements[i] = (struct plane*)malloc(sizeof(struct plane*));
+                q->elements[i] = (struct plane*)malloc(sizeof(struct plane));
         }
+        printf("[QUEUE] Buffer initialized\n");
 
         return 0;
 }
 
 /* To Enqueue an element*/
 int queue_put(struct plane* x) {
+        printf("[QUEUE] Storing plane with id %d\n", x->id_number);
+        pthread_mutex_lock(&queue_lock);
+
+        while (flag == 1) {
+                pthread_cond_wait(&queue_cond, &queue_lock);
+        }
+        flag = 1;
+
         if (queue_full() == 1) {
                 printf("Full queue\n");
+                flag = 0;
+                pthread_cond_signal(&queue_cond);
+                pthread_mutex_unlock(&queue_lock);
                 return -1;
         } else if (q->front == -1) {
                 q->front = q->rear = 0;
                 q->elements[q->rear] = x;
-                //memcpy(q->elements[q->rear], x, sizeof(struct plane));
         } else if (q->rear == q->size-1 && q->front != 0) {
                 q->rear = 0;
                 q->elements[q->rear] = x;
-                //memcpy(q->elements[q->rear], x, sizeof(struct plane));
         } else {
                 q->rear++;
                 q->elements[q->rear] = x;
-                //memcpy(q->elements[q->rear], x, sizeof(struct plane));
         }
+
+        flag = 0;
+        pthread_cond_signal(&queue_cond);
+        pthread_mutex_unlock(&queue_lock);
         return 0;
 }
 
 
 /* To Dequeue an element.*/
 struct plane* queue_get(void) {
+        pthread_mutex_lock(&queue_lock);
+        while (flag == 1) {
+                pthread_cond_wait(&queue_cond, &queue_lock);
+        }
+        flag = 1;
+
         if (queue_empty() == 1) {
                 printf("Empty queue\n");
+                flag = 0;
+                pthread_cond_signal(&queue_cond);
+                pthread_mutex_unlock(&queue_lock);
                 return NULL;
         }
 
@@ -69,6 +97,11 @@ struct plane* queue_get(void) {
         } else {
                 q->front++;
         }
+        printf("[QUEUE] Getting plane with id %d\n", element->id_number);
+
+        flag = 0;
+        pthread_cond_signal(&queue_cond);
+        pthread_mutex_unlock(&queue_lock);
 
         return element;
 }
@@ -76,23 +109,47 @@ struct plane* queue_get(void) {
 
 /*To check queue state*/
 int queue_empty(void){
+        // pthread_mutex_lock(&queue_lock);
+        // while (flag == 1) {
+        //         pthread_cond_wait(&queue_cond, &queue_lock);
+        // }
+        // flag = 1;
+
         if (q->front == -1) {
                 /* Queue empty */
                 printf("Empty queue\n");
+                // flag = 0;
+                // pthread_cond_signal(&queue_cond);
+                // pthread_mutex_unlock(&queue_lock);
                 return 1;
         }
         /* Queue not empty */
+        // flag = 0;
+        // pthread_cond_signal(&queue_cond);
+        // pthread_mutex_unlock(&queue_lock);
         return 0;
 }
 
 /*To check queue state*/
 int queue_full(void){
+        // pthread_mutex_lock(&queue_lock);
+        // while (flag == 1) {
+        //         pthread_cond_wait(&queue_cond, &queue_lock);
+        // }
+        // flag = 1;
+
         if ((q->front == 0 && q->rear == q->size-1) ||
             (q->rear == (q->front-1)%(q->size-1))) {
                 printf("Full queue\n");
+                // flag = 0;
+                // pthread_cond_signal(&queue_cond);
+                // pthread_mutex_unlock(&queue_lock);
                 return 1;
         }
         /* Queue not full */
+        // flag = 0;
+        // pthread_cond_signal(&queue_cond);
+        // pthread_mutex_unlock(&queue_lock);
         return 0;
 }
 
@@ -112,33 +169,33 @@ int queue_destroy(void){
 }
 
 void print_plane(struct plane* pln) {
-  printf("id_number = %d\n", pln->id_number);
-  printf("time_action = %d\n", pln->time_action);
-  printf("action = %d\n", pln->action);
-  printf("last_flight = %d\n", pln->last_flight);
+        printf("id_number = %d\n", pln->id_number);
+        printf("time_action = %d\n", pln->time_action);
+        printf("action = %d\n", pln->action);
+        printf("last_flight = %d\n", pln->last_flight);
 }
 
 void display_queue(void) {
-  int i;
-  if (queue_empty() == 1) {
-    return;
-  }
+        int i;
+        if (queue_empty() == 1) {
+                return;
+        }
 
-  printf("Elements in circular queue are:\n");
-  if (q->rear >= q->front) {
-    for (i = q->front; i <= q->rear; i++) {
-      printf("PLANE IN POSITION %d\n", i);
-      print_plane(q->elements[i]);
-    }
-  } else {
-    for (i = q->front; i < q->size; i++) {
-      printf("PLANE IN POSITION %d\n", i);
-      print_plane(q->elements[i]);
-    }
-    for (i = 0; i <= q->rear; i++) {
-      printf("PLANE IN POSITION %d\n", i);
-      print_plane(q->elements[i]);
-    }
-  }
+        printf("Elements in circular queue are:\n");
+        if (q->rear >= q->front) {
+                for (i = q->front; i <= q->rear; i++) {
+                        printf("PLANE IN POSITION %d\n", i);
+                        print_plane(q->elements[i]);
+                }
+        } else {
+                for (i = q->front; i < q->size; i++) {
+                        printf("PLANE IN POSITION %d\n", i);
+                        print_plane(q->elements[i]);
+                }
+                for (i = 0; i <= q->rear; i++) {
+                        printf("PLANE IN POSITION %d\n", i);
+                        print_plane(q->elements[i]);
+                }
+        }
 
 }

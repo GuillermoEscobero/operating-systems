19fce59a6f808c238a644d3514073c1feb22c01f
RaulOlmedoCheca <raul.olmedo.checa@gmail.com>
2018-05-02 20:27:34 +0200

    Commented code

--- arcport.c
+++ arcport.c
@@ -17,7 +17,7 @@
 #define DEFAULT_TIME_LANDING 3
 #define DEFAULT_SIZE 6
 
-pthread_mutex_t main_mutex;
+pthread_mutex_t main_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int planes_takeoff = DEFAULT_PLANES_TAKEOFF;
 int time_takeoff = DEFAULT_TIME_TAKEOFF;
@@ -41,24 +41,34 @@ void print_end() {
 }
 
 int serve_landing(struct plane *pln) {
+    /* Land the plane taken as parameter and print the messages required */
     printf("[CONTROL] Track is free for plane with id %d\n", pln->id_number);
+
+    /* If the plane is the last one, print the message showing it */
     if (pln->last_flight == 1) {
         printf("[CONTROL] After plane with id %d the airport will be closed\n", pln->id_number);
     }
+    /* Simulate the landing time using sleep() with the duration in the plane structure */
     sleep((unsigned int) pln->time_action);
-    printf("[CONTROL] Plane %d landed in %d seconds$$$$$$$$$$$$$$$$$$$\n", pln->id_number, pln->time_action);
+    printf("[CONTROL] Plane %d landed in %d seconds\n", pln->id_number, pln->time_action);
+    /* Increase the counter of planes landed to track them in order to print later a file with the info */
     served_landings++;
 
     return 0;
 }
 
 int serve_takeoff(struct plane *pln) {
+    /* Takeoff the plane taken as parameter and print the messages required */
     printf("[CONTROL] Putting plane with id %d in track\n", pln->id_number);
+
+    /* If the plane is the last one, print the message showing it */
     if (pln->last_flight == 1) {
         printf("[CONTROL] After plane with id %d the airport will be closed\n", pln->id_number);
     }
+    /* Simulate the takeoff time using sleep() with the duration in the plane structure */
     sleep((unsigned int) pln->time_action);
-    printf("[CONTROL] Plane %d took off after %d seconds$$$$$$$$$$$$$$$$$\n", pln->id_number, pln->time_action);
+    printf("[CONTROL] Plane %d took off after %d seconds\n", pln->id_number, pln->time_action);
+    /* Increase the counter of planes that took off to track them in order to print later a file with the info */
     served_takeoffs++;
 
     return 0;
@@ -66,23 +76,32 @@ int serve_takeoff(struct plane *pln) {
 
 void track_manager(void) {
     int i;
+    /* Loop through the number of planes to be created received as arguments */
     for (i = 0; i < planes_takeoff; i++) {
+        /* Protect the creation of the plane in order to be able to assure that
+         * no other one has the same id as the one being created by this thread */
         pthread_mutex_lock(&main_mutex);
+
         struct plane *pln = (struct plane *) malloc(sizeof(struct plane));
         pln->id_number = next_id;
         pln->time_action = time_takeoff;
         pln->action = OP_TAKEOFF;
 
+        /* If the plane is the last one, set the last_flight flag to one */
         if (next_id == (planes_land + planes_takeoff - 1)) {
             pln->last_flight = 1;
         } else {
             pln->last_flight = 0;
         }
+        /* Increment the plane counter and print the message required */
         next_id++;
         printf("[TRACKBOSS] Plane with id %d checked\n", pln->id_number);
 
+        /* Store the plane into the queue and print the required message */
         queue_put(pln);
         printf("[TRACKBOSS] Plane with id %d ready to takeoff\n", pln->id_number);
+
+        /* Unlock the mutex to allow other threads to create more planes */
         pthread_mutex_unlock(&main_mutex);
 
     }
@@ -91,22 +110,32 @@ void track_manager(void) {
 
 void radar(void) {
     int i;
+    /* Loop through the number of planes to be created received as arguments */
     for (i = 0; i < planes_land; i++) {
+        /* Protect the creation of the plane in order to be able to assure that
+         * no other one has the same id as the one being created by this thread */
         pthread_mutex_lock(&main_mutex);
+
         struct plane *pln = (struct plane *) malloc(sizeof(struct plane));
         pln->id_number = next_id;
         pln->time_action = time_landing;
         pln->action = OP_LAND;
 
+        /* If the plane is the last one, set the last_flight flag to one */
         if (pln->id_number == (planes_land + planes_takeoff - 1)) {
             pln->last_flight = 1;
         } else {
             pln->last_flight = 0;
         }
+        /* Increment the plane counter and print the message required */
         next_id++;
         printf("[RADAR] Plane with id %d detected!\n", pln->id_number);
+
+        /* Store the plane into the queue and print the required message */
         queue_put(pln);
         printf("[RADAR] Plane with id %d ready to land\n", pln->id_number);
+
+        /* Unlock the mutex to allow other threads to create more planes */
         pthread_mutex_unlock(&main_mutex);
     }
     pthread_exit(0);
@@ -114,10 +143,14 @@ void radar(void) {
 
 
 void tower(void) {
+    /* Initialize the flag to 0 */
     int last_flight = 0;
+
+    /* Loop until the last plane is served */
     while (last_flight == 0) {
         struct plane *pln;
 
+        /* Get the first plane from the queue */
         pln = queue_get();
 
         if (pln == NULL) {
@@ -125,12 +158,14 @@ void tower(void) {
             pthread_exit(0);
         }
 
+        /* Depending on where is the plane (air or land) takeoff or land it */
         if (pln->action == OP_TAKEOFF) {
             serve_takeoff(pln);
         } else {
             serve_landing(pln);
         }
 
+        /* Update the flag to the one in the plane served in this iteration, if it is the last one, the loop will end */
         last_flight = pln->last_flight;
         free(pln);
 
@@ -152,6 +187,7 @@ int main(int argc, char **argv) {
 
     print_banner();
 
+    /* Parse arguments */
     if (argc != 1) {
         planes_takeoff = (int) strtol(argv[1], NULL, 10);
         time_takeoff = (int) strtol(argv[2], NULL, 10);
@@ -160,21 +196,23 @@ int main(int argc, char **argv) {
         size = (int) strtol(argv[5], NULL, 10);
     }
 
-    pthread_mutex_init(&main_mutex, NULL);
-
+    /* Initialize the circular queue */
     queue_init(size);
 
-    pthread_t pid[3];
-    pthread_create(&pid[0], NULL, (void *) track_manager, (void *) &planes_takeoff);
-    pthread_create(&pid[1], NULL, (void *) radar, (void *) &planes_land);
-    pthread_create(&pid[2], NULL, (void *) tower, NULL);
+    /* Create an array of threads to save the thread id and create three of them with a function to execute */
+    pthread_t tid[3];
+    pthread_create(&tid[0], NULL, (void *) track_manager, (void *) &planes_takeoff);
+    pthread_create(&tid[1], NULL, (void *) radar, (void *) &planes_land);
+    pthread_create(&tid[2], NULL, (void *) tower, NULL);
 
-    pthread_join(pid[0], NULL);
-    pthread_join(pid[1], NULL);
-    pthread_join(pid[2], NULL);
+    /* Join the threads after they exit */
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+    pthread_join(tid[2], NULL);
 
     print_end();
 
+    /* Destroy the queue and the mutex */
     queue_destroy();
     pthread_mutex_destroy(&main_mutex);
 
